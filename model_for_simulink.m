65a4bed09547b0d1a2e8164450f603ccb254e187
Andrew Conlin <aconlin99@gmail.com>
2021-04-06 17:09:07 +0100

    Updated code + added comments

--- model_for_simulink.m
+++ model_for_simulink.m
@@ -1,6 +1,6 @@
 clear all; % Clear all variables
 clc; % Clear command line
-load('trainedModel_linear.mat') % Load desired model
+load('trainedModel_linear_adapt.mat') % Load desired model
 load('main_scaling_factors.mat') % Load appropriate scaling factors
-Mdl = trainedModel_linear.LinearModel; % Save model to Mdl
+Mdl = trainedModel_linear_adapt.LinearModel; % Save model to Mdl
 saveLearnerForCoder(Mdl,'model'); % Save in a format that can be interpreted by Simulink
--- plotter.m
+++ plotter.m
@@ -1,23 +1,23 @@
 figure(1);
 yyaxis left; % Select left y axis
-a = plot(out.tout,out.phi,out.tout,out.x); % Plot to x axis and left y axis
+a = plot(out.tout,out.u); % Plot to x axis and left y axis
 grid on;
-title('Deviation of alternative pendulum from upright and X position of cart (Non-optimal P.I.D)') % Title
+title('Angle, X position and force on cart (Linear regression, force unrestricted)') % Title
 % Labels and limits
 xlabel('Time (s)');
 xlim([0 30]);
-ylabel('Angle and X position (rad and m)');
-ylim([-0.6 0.6]);
+ylabel('Force on cart (N)');
+%ylim([-0.6 0.6]);
 
 
 yyaxis right; % Select right y axis
-b = plot(out.tout,out.u); % Plot to x axis and right y axis
+b = plot(out.tout,out.phi,out.tout,out.x); % Plot to x axis and right y axis
 % Labels, ranges and stylings
-ylabel('Force on cart');
-ylim([60 -10]);
-b.LineWidth = 1.3;
-l = refline(0,0);
-l.Color = 'k';
-l.LineStyle = ':';
-legend('Angle (rad)','X (m)','0 reference','Location','southeast');
+ylabel('Deviation of pendulum (rad) and X position of cart (m)');
+% ylim([-70 70]);
+% b.LineWidth = 1.3;
+% l = refline(0,0);
+% l.Color = 'k';
+% l.LineStyle = ':';
+legend('Angle (rad)','X (m)','Force (N)','Location','southeast');
 
